399bf2a1815c28a063dcf94a1e406a416d4d6a09
Dz0526 <this@dz99.me>
2024-11-28 01:44:34 +0900

    feat: add gpu page detail

---
+++ app/_consists/abi.ts
@@ -0,0 +1,196 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "goodAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "badAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "speedAmount",
+        type: "uint256",
+      },
+    ],
+    name: "addEvaluate",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "storageAmount",
+        type: "uint256",
+      },
+    ],
+    name: "addStorageEvaluate",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAllEvaluations",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getBad",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getEvaluations",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getGood",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "getSpeed",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
--- app/gpu/page.tsx
+++ app/gpu/page.tsx
@@ -1,10 +1,64 @@
-import { Container } from "@mui/material";
+import {
+  Box,
+  Container,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
 import { PrimaryTypography } from "../_component/PrimaryTypography";
 import Link from "next/link";
+import { ethers, Result } from "ethers";
+import { abi } from "../_consists/abi";
 
-const GPU = () => {
+const addressIndex = 0;
+const goodIndex = 1;
+const badIndex = 2;
+const speedIndex = 3;
+
+export const fetchBlockchain = async () => {
+  const provider = new ethers.JsonRpcProvider(
+    "https://rpc-amoy.polygon.technology"
+  );
+  const contract = new ethers.Contract(
+    "0x322527d78D5B297d5cC0be9E8a75A2701c661FDB",
+    abi,
+    provider
+  );
+  const result: Result[][] = await contract.getAllEvaluations();
+  const blockchains = result[addressIndex].map((val, idx) => {
+    return {
+      address: val,
+      good: Number(result[goodIndex][idx]),
+      bad: Number(result[badIndex][idx]),
+      speed: Number(result[speedIndex][idx]),
+    };
+  });
+  return blockchains.sort((a, b) => {
+    const aEvaluation = a.good + a.speed - a.bad;
+    const bEvaluation = b.good + b.speed - b.bad;
+    if (aEvaluation > bEvaluation) {
+      return -1;
+    }
+    if (aEvaluation < bEvaluation) {
+      return 1;
+    }
+    return 0;
+  });
+};
+
+const GPU = async () => {
+  const blockchains = await fetchBlockchain();
   return (
-    <Container maxWidth={"md"} sx={{ pt: "16px" }}>
+    <Container
+      maxWidth={"lg"}
+      sx={{ pt: "16px", display: "flex", flexDirection: "column", gap: "32px" }}
+    >
       <Link
         href={
           "https://gossamer-lion-194.notion.site/GPU-810a6331c78749d4a4879dce02076273?pvs=4"
@@ -18,6 +72,38 @@ const GPU = () => {
           新しくGPU投資する方へ
         </PrimaryTypography>
       </Link>
+      <Box>
+        <Stack flexDirection={"row"} alignItems={"center"} gap={"16px"}>
+          <PrimaryTypography fontWeight={"bold"} fontSize={"20px"}>
+            最新のGPU評価
+          </PrimaryTypography>
+          <Typography color="#b8b8b8" fontSize={"20px"} fontWeight={"semibold"}>
+            ※評価の高い順に並べます
+          </Typography>
+        </Stack>
+        <TableContainer component={Paper} sx={{ mt: "8px" }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Wallet Address</TableCell>
+                <TableCell>Good</TableCell>
+                <TableCell>Bad</TableCell>
+                <TableCell>Speed</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {blockchains.map((blockchain) => (
+                <TableRow key={String(blockchain.address)}>
+                  <TableCell scope="row">{blockchain.address}</TableCell>
+                  <TableCell>{blockchain.good}</TableCell>
+                  <TableCell>{blockchain.bad}</TableCell>
+                  <TableCell>{blockchain.speed}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Container>
   );
 };
