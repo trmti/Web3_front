45ab9f37ddd83671be4f7f4076eedfb7e5b48e3c
Dz0526 <this@dz99.me>
2024-12-06 21:09:36 +0900

    feat: add create model button when empty

--- app/_component/PrimaryButton.tsx
+++ app/_component/PrimaryButton.tsx
@@ -3,15 +3,7 @@ import { Button, ButtonProps } from "@mui/material";
 export const PrimaryButton = ({ children, sx, ...rest }: ButtonProps) => {
   return (
     <Button
-      variant="outlined"
       color="secondary"
-      // onClick={handleSignUpClick}
-      // sx={{
-      //   width: "300px",
-      //   borderRadius: "5px",
-      //   border: "1px solid #3483d7",
-      //   fontWeight: "bold",
-      // }}
       sx={{
         mb: 2,
         width: "300px",
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,6 +11,8 @@ import SendIcon from "@mui/icons-material/Send";
 import { useRouter } from "next/navigation";
 import EditIcon from "@mui/icons-material/Edit";
 import { useQuery } from "@tanstack/react-query";
+import { PrimaryTypography } from "../_component/PrimaryTypography";
+import { PrimaryButton } from "../_component/PrimaryButton";
 
 type ModelResponse = {
   models: {
@@ -36,7 +38,7 @@ export default function Home() {
       ).then((res) => res.json()),
   });
 
-  return (
+  return data && data.models.length !== 0 ? (
     <Container
       maxWidth="md"
       sx={{
@@ -47,57 +49,79 @@ export default function Home() {
         marginTop: 5,
       }}
     >
-      {data &&
-        data.models.map((value, index) => (
-          <Card
-            key={index}
-            sx={{
-              minWidth: 200,
-              flex: "1 1 calc(25.0% - 16px)",
-              boxSizing: "border-box",
-              color: "#dadfe8",
-            }}
-          >
-            <CardContent>
+      {data.models.map((value, index) => (
+        <Card
+          key={index}
+          sx={{
+            minWidth: 200,
+            flex: "1 1 calc(25.0% - 16px)",
+            boxSizing: "border-box",
+            color: "#dadfe8",
+          }}
+        >
+          <CardContent>
+            <Typography
+              sx={{ color: "#373e5a", mb: 2.0 }}
+              variant="h5"
+              component="div"
+            >
+              {value.name}
+            </Typography>
+
+            {value.base_models.map((value, index) => (
               <Typography
-                sx={{ color: "#373e5a", mb: 2.0 }}
-                variant="h5"
-                component="div"
+                key={value.id}
+                sx={{ color: "text.secondary", mb: 1.0 }}
               >
                 {value.name}
               </Typography>
-
-              {value.base_models.map((value, index) => (
-                <Typography
-                  key={value.id}
-                  sx={{ color: "text.secondary", mb: 1.0 }}
-                >
-                  {value.name}
-                </Typography>
-              ))}
-            </CardContent>
-            <CardActions>
-              <Button
-                size="small"
-                disabled={!value.train_completed}
-                sx={{ ":disabled": { opacity: 0.3 } }}
-                onClick={() => router.push(`/chat-room/${value.id}`)}
-              >
-                <SendIcon sx={{ color: "#373e5a" }}></SendIcon>{" "}
-                <span style={{ color: "#373e5a", marginLeft: 5 }}>CHAT</span>
-              </Button>
-              <Button
-                size="small"
-                disabled={!value.train_completed}
-                sx={{ ":disabled": { opacity: 0.3 } }}
-                onClick={() => router.push("/learn")}
-              >
-                <EditIcon sx={{ color: "#373e5a" }}></EditIcon>{" "}
-                <span style={{ color: "#373e5a", marginRight: 1 }}>learn</span>
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
+            ))}
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              disabled={!value.train_completed}
+              sx={{ ":disabled": { opacity: 0.3 } }}
+              onClick={() => router.push(`/chat-room/${value.id}`)}
+            >
+              <SendIcon sx={{ color: "#373e5a" }}></SendIcon>{" "}
+              <span style={{ color: "#373e5a", marginLeft: 5 }}>CHAT</span>
+            </Button>
+            <Button
+              size="small"
+              disabled={!value.train_completed}
+              sx={{ ":disabled": { opacity: 0.3 } }}
+              onClick={() => router.push("/learn")}
+            >
+              <EditIcon sx={{ color: "#373e5a" }}></EditIcon>{" "}
+              <span style={{ color: "#373e5a", marginRight: 1 }}>learn</span>
+            </Button>
+          </CardActions>
+        </Card>
+      ))}
+    </Container>
+  ) : (
+    <Container
+      maxWidth={"md"}
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "32px",
+        marginTop: "64px",
+      }}
+    >
+      <PrimaryTypography fontSize={"48px"}>
+        さぁ、モデルを作成しましょう！
+      </PrimaryTypography>
+      <PrimaryButton
+        onClick={() => {
+          router.push("/model_select");
+        }}
+      >
+        新規作成
+      </PrimaryButton>
     </Container>
   );
 }
